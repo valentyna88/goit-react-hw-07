cdd7a972af1db791e0ab3c162e2d99b44e1e819a
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-10-31 19:06:22 +0200

    Uninstall redux-persist

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,12 @@ import 'modern-normalize';
 import './index.css';
 import App from './components/App/App';
 import { Provider } from 'react-redux';
-import store, { persistor } from './redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
+import store from './redux/store';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
   </StrictMode>
 );
