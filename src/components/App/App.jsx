6ba87ae73591eabfcc67606964ec5491b6c72fb4
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-11-01 14:19:05 +0200

    Update contactsOps and App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,8 +2,16 @@ import css from './App.module.css';
 import ContactForm from '../ContactForm/ContactForm';
 import SearchBox from '../SearchBox/SearchBox';
 import ContactList from '../ContactList/ContactList';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchContacts } from '../../redux/contactsOps';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts);
+  }, [dispatch]);
   return (
     <div className={css.container}>
       <h1 className={css.title}>Phonebook</h1>
--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -3,7 +3,14 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 axios.defaults.baseURL = 'https://6723b093493fac3cf24bee0c.mockapi.io';
 
-export const fetchContacts = createAsyncThunk('contacts/fetchAll', async () => {
-  const { data } = await axios.get('contacts');
-  return data;
-});
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchAll',
+  async (_, thunkAPI) => {
+    try {
+      const { data } = await axios.get('contacts');
+      return data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
