c388f94a64277a5b2af4ed3c86c423b76317709c
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-11-01 14:08:59 +0200

    Update contactsSlice

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { fetchContacts } from './contactsOps';
 
 const INITIAL_STATE = {
   items: [],
@@ -9,7 +10,21 @@ const INITIAL_STATE = {
 const contactsSlice = createSlice({
   name: 'contacts',
   initialState: INITIAL_STATE,
-  extraReducers: builder => builder,
+  extraReducers: builder => {
+    builder
+      .addCase(fetchContacts.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(fetchContacts.fulfilled, (state, action) => {
+        state.isLoading = true;
+        state.error = null;
+        state.items = action.payload;
+      })
+      .addCase(fetchContacts.rejected, (state, action) => {
+        state.isLoading = true;
+        state.error = action.payload;
+      });
+  },
 });
 export const contactsReducer = contactsSlice.reducer;
 export const { addContact, deleteContact } = contactsSlice.actions;
